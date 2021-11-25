16d12c0538be1fd9fb3f53911555c05a78fec1f1
PedrorFelix <pedrojrfelix1999@gmail.com>
2021-11-25 22:29:04 +0000

    getters e setters

--- src/classes/Nave.java
+++ src/classes/Nave.java
@@ -7,4 +7,48 @@ public class Nave {
 	private static int dimensoes[];
 	private static int massa;
 	private static int potencia;
+	
+	
+	
+	
+	public static int getId() {
+		return id;
+	}
+	public static void setId(int id) {
+		Nave.id = id;
+	}
+	public static String getNome() {
+		return nome;
+	}
+	public static void setNome(String nome) {
+		Nave.nome = nome;
+	}
+	public static int[] getDimensoes() {
+		return dimensoes;
+	}
+	public static void setDimensoes(int[] dimensoes) {
+		Nave.dimensoes = dimensoes;
+	}
+	public static int getMassa() {
+		return massa;
+	}
+	public static void setMassa(int massa) {
+		Nave.massa = massa;
+	}
+	public static int getPotencia() {
+		return potencia;
+	}
+	public static void setPotencia(int potencia) {
+		Nave.potencia = potencia;
+	}
+	public static String[] getProtocolos() {
+		return protocolos;
+	}
+	public static void setProtocolos(String[] protocolos) {
+		Nave.protocolos = protocolos;
+	}
+	private static String protocolos[];
+	
+	
+	
 }
